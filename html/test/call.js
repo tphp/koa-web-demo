597a3b1525d469d9b67a8a3b93f2b009292b5285
tphp <336296@qq.com>
2021-08-26 19:58:57 +0800

    call

--- html/test/call.js
+++ html/test/call.js
@@ -1,5 +1,10 @@
 module.exports = async (hd) => {
   return await hd.call(
+    // 与hd.http模式不同
+    // hd.call 内部访问
+    // hd.http 外部访问
+    // 可以带参数访问，如:
+    // test/data.js?say=hello
     'test/data',
     {
       sayWorld: "MyWorld"
