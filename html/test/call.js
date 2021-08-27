c92f70d58b4965b3ef972bbba9e37639f4973a93
tphp <336296@qq.com>
2021-08-27 19:11:44 +0800

    new

--- html/test/call.js
+++ html/test/call.js
@@ -1,11 +1,15 @@
 module.exports = async (hd) => {
   return await hd.call(
     // 与hd.http模式不同
-    // hd.call 内部访问
-    // hd.http 外部访问
+    // hd.call 内部访问 (相对路径、绝对路径)
+    // hd.http 外部访问 (url模式)
     // 可以带参数访问，如:
-    // test/data.js?say=hello
-    'test/data',
+    // /test/data.js?say=hello
+    // data 解析为：/test/data
+    // ./data 解析为：/test/data
+    // ../data 解析为：/data
+    // $abc 解析为：/test/dataabc
+    '/test/data',
     {
       sayWorld: "MyWorld"
     }
